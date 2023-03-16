6611053f33f95cca6c4bdb8fac799da199efa2db
Joe <lizhou1021@qq.com>
2023-03-16 21:57:01 +0800

    feat: 下载资源到本地并且读取

--- App.tsx
+++ App.tsx
@@ -19,6 +19,7 @@ import {
   Button,
   Linking,
   Platform,
+  Image,
 } from 'react-native';
 
 import {
@@ -34,6 +35,58 @@ import Server, {
 
 import { WebView } from 'react-native-webview';
 
+export const formUrl = 'https://static.runoob.com/images/demo/demo1.jpg';
+export const downloadDest = `${RNFS.DocumentDirectoryPath}/webroot/demo.jpg`;
+
+/*下载文件*/
+export function downloadFile(downloadDest: string, formUrl: string) {
+  // On Android, use "RNFS.DocumentDirectoryPath" (MainBundlePath is not defined)
+
+  // 图片
+  // const downloadDest = `${RNFS.MainBundlePath}/${((Math.random() * 1000) | 0)}.jpg`;
+  // const formUrl = 'http://img.kaiyanapp.com/c7b46c492261a7c19fa880802afe93b3.png?imageMogr2/quality/60/format/jpg';
+
+  // 文件
+  // const downloadDest = `${RNFS.MainBundlePath}/${((Math.random() * 1000) | 0)}.zip`;
+  // const formUrl = 'http://files.cnblogs.com/zhuqil/UIWebViewDemo.zip';
+
+  // 视频
+  // const downloadDest = `${RNFS.MainBundlePath}/${((Math.random() * 1000) | 0)}.mp4`;
+  // http://gslb.miaopai.com/stream/SnY~bbkqbi2uLEBMXHxGqnNKqyiG9ub8.mp4?vend=miaopai&
+  // https://gslb.miaopai.com/stream/BNaEYOL-tEwSrAiYBnPDR03dDlFavoWD.mp4?vend=miaopai&
+  // const formUrl = 'https://gslb.miaopai.com/stream/9Q5ADAp2v5NHtQIeQT7t461VkNPxvC2T.mp4?vend=miaopai&';
+
+  // http://wvoice.spriteapp.cn/voice/2015/0902/55e6fc6e4f7b9.mp3
+  // const formUrl = 'http://wvoice.spriteapp.cn/voice/2015/0818/55d2248309b09.mp3';
+
+  const options = {
+    fromUrl: formUrl,
+    toFile: downloadDest,
+    background: true,
+    begin: res => {
+      console.log('begin', res);
+      console.log('contentLength:', res.contentLength / 1024 / 1024, 'M');
+    },
+    progress: res => {
+      let pro = res.bytesWritten / res.contentLength;
+      console.log('pro: ', pro);
+    },
+  };
+  try {
+    const ret = RNFS.downloadFile(options);
+    ret.promise
+      .then(res => {
+        console.log('success', res);
+
+        console.log('file://' + downloadDest);
+      })
+      .catch(err => {
+        console.log('err', err);
+      });
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -79,7 +132,7 @@ function App(): JSX.Element {
         }
         if (extract) {
           console.log('Extracting web server assets...');
-          await extractBundledAssets(fileDir, 'webroot');
+          // await extractBundledAssets(fileDir, 'webroot');
         }
       }
 
@@ -95,7 +148,7 @@ function App(): JSX.Element {
         console.log(`New server state is "${STATES[newState]}"`);
       });
       const res = await server?.start();
-      console.log('res: ', res);
+
       if (res && server) {
         setOrigin(res);
       }
@@ -113,9 +166,19 @@ function App(): JSX.Element {
 
   const webView = useRef<WebView>(null);
 
+  console.log(origin);
 
   return (
     <View style={styles.webview}>
+      <View style={{ height: 50, display: "flex" }}>
+        <Button title='下载文件' onPress={() => downloadFile(downloadDest, formUrl)}></Button>
+        {
+          origin && <Image
+            style={styles.tinyLogo}
+            source={{ uri: origin + "/demo.jpg" }}
+          />
+        }
+      </View>
       <WebView
         style={{ flex: 1 }}
         cacheMode="LOAD_NO_CACHE"
@@ -141,7 +204,7 @@ function App(): JSX.Element {
         ref={webView}
         source={{ uri: origin }}
       />
-    </View>
+    </View >
   );
 }
 
@@ -152,6 +215,11 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 12,
   },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
 });
 
 export default App;
+
